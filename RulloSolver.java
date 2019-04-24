e05c8258af07c74b4bc7be52ac07b30dadececdc
Garrett Bennett <garrettbennett@centraloffice.opelikaschools.org>
2019-04-24 10:19:23 -0500

    initial commit

---
+++ RulloSolver.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class RulloSolver{
+    boolean[][] active = new boolean[5][5];
+    
+
+    
+    
+    
+    
+    public static void main(String args[ ]){
+        System.out.println("hi");
+    }
+}
