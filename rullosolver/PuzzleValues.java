55368e5e359d7150c85457b47dfabdb57966fc5c
gbennett00 <bennett.g00@gmail.com>
2020-04-21 09:08:59 -0500

    changed to 0-4

---
+++ rullosolver/PuzzleValues.java
@@ -0,0 +1,46 @@
+//package rullosolver;
+
+public class PuzzleValues{
+    public static final int COLLUMN_ONE = 17;
+    public static final int COLLUMN_TW0 = 4;
+    public static final int COLLUMN_THREE = 20;
+    public static final int COLLUMN_FOUR = 1;
+    public static final int COLLUMN_FIVE = 24;
+    public static final int ROW_ONE = 6;
+    public static final int ROW_TWO = 17;
+    public static final int ROW_THREE = 16;
+    public static final int ROW_FOUR = 14;
+    public static final int ROW_FIVE = 13;
+
+    //need to rewrite from 0-4 instead of 1-5   im coming home coming home 
+    //sums[row#][collumn#]                      tell the world im coming home
+    static int[][] sums = new int[5][5];  
+    sums[0][0] = 5;
+    sums[0][1] = 7; //wtf bro
+    sums[0][2] = 6;
+    sums[0][3] = 1;
+    sums[0][4]= 1;
+    sums[1][0] = 3;
+    sums[1][1] = 1;
+    sums[1][2] = 4;
+    sums[1][3] = 1;
+    sums[1][4] = 9;
+    sums[2][0] = 6;
+    sums[2][1] = 1;
+    sums[2][2] = 5;
+    sums[2][3] = 3;
+    sums[2][4] = 9;
+    sums[3][0] = 7;
+    sums[3][1] = 2;
+    sums[3][2] = 7;
+    sums[3][3] = 9;
+    sums[3][4] = 5;
+    sums[4][0] = 3;
+    sums[4][1] = 1;
+    sums[4][2] = 9;
+    sums[4][3] = 7;
+    sums[4][4] = 9;
+
+
+    
+}
--- rullosolver/RulloSolver.java
+++ rullosolver/RulloSolver.java
@@ -1,11 +1,12 @@
 package rullosolver;
 
+import rullosolver.PuzzleValues;
 import java.util.*;
 import java.io.*;
 
 public class RulloSolver{
     static boolean[][] active = new boolean[5][5];
-    static int[][] sums = new int[5][5];
+    
     
 
     
@@ -13,6 +14,7 @@ public class RulloSolver{
     
     
     public static void main(String args[ ]){
-        System.out.println(active[4][0]);
+        //System.out.println(active[4][0]);
+        System.out.println(PuzzleValues.COLLUMN_THREE);
     }
 }
