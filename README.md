d44727e14e12111bbb992df0031fad82e226dc45
gbennett00 <bennett.g00@gmail.com>
2020-05-07 21:30:42 -0500

    clean up

--- README.md
+++
@@ -1,1 +0,0 @@
-# Rullo-Solver
--- rullosolver/RulloSolver.java
+++ rullosolver/RulloSolver.java
@@ -1,20 +1,11 @@
 package rullosolver;
 
-import rullosolver.PuzzleValues;
 import java.util.*;
 import java.io.*;
 
 public class RulloSolver{
-    static boolean[][] active = new boolean[5][5];
-    
-    
-
-    
-    
-    
-    
+  
     public static void main(String args[ ]){
-        //System.out.println(active[4][0]);
-        System.out.println(PuzzleValues.COLLUMN_THREE);
+        System.out.println(PuzzleValues.sums[2][4]);
     }
 }
